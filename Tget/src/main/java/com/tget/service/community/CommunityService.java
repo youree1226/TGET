6bf9af77054123ce45211c034a846cd16fb5f216
user <user@DESKTOP-74IMAPF>
2019-07-09 20:28:20 +0900

    userJY: Domain, Service, Dao 완료/ Imp 수정필요

--- Tget/src/main/java/com/tget/service/community/CommunityService.java
+++ Tget/src/main/java/com/tget/service/community/CommunityService.java
@@ -1,5 +1,33 @@
 package com.tget.service.community;
 
-public interface CommunityService {
+import java.util.Map;
 
+import com.tget.common.domain.Search;
+import com.tget.service.community.domain.Content;
+import com.tget.service.community.domain.Report;
+
+public interface CommunityService {
+	
+	
+		public void addContent(Content content) throws Exception;
+
+		public void addReport(Report report) throws Exception;
+		
+		public Content getContent(int contentNo) throws Exception;
+
+		public Map<String,Object> getContentList(Search search) throws Exception;
+		
+		public Map<String,Object> getReplyList(Search search) throws Exception;
+
+		public Map<String,Object> getReportList(Search search) throws Exception; //getTotalCount, getReportList 
+		
+		public void updateContent(Content content) throws Exception;
+		
+		public void updateGoodCount(Map<String,Object> goodCount)throws Exception;
+		
+		public void updateBadCount(Map<String,Object> badCount)throws Exception;
+		
+		public void updateRefund(Content content) throws Exception;
+		
+		//날씨 어떻게 할지....
 }
