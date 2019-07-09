773e8481403efd4ffb3a9dfed1225845571be052
user <user@DESKTOP-74IMAPF>
2019-07-09 20:28:51 +0900

    user04 : 07.09 좋았다

--- Tget/src/main/java/com/tget/service/coupon/CouponDao.java
+++ Tget/src/main/java/com/tget/service/coupon/CouponDao.java
@@ -2,13 +2,12 @@ package com.tget.service.coupon;
 
 import com.tget.service.coupon.domain.Coupon;
 
-public interface CouponService {
+public interface CouponDao {
 
-	public void addCoupon(Coupon coupon) throws Exception;
+	public void insertCoupon(Coupon coupon) throws Exception;
 
-	public Coupon getCoupon(int couponNo) throws Exception;
+	public Coupon selectCoupon(int couponNo) throws Exception;
 	
 	public void updateCoupon(Coupon coupon) throws Exception;
-	
 	
 }
--- Tget/src/main/java/com/tget/service/user/UserService.java
+++ Tget/src/main/java/com/tget/service/user/UserService.java
@@ -1,5 +1,43 @@
 package com.tget.service.user;
 
-public interface UserService {
+import java.util.Map;
 
+import com.tget.common.domain.Search;
+import com.tget.service.user.domain.User;
+
+
+public interface UserService {
+	//회원가입
+	public void addUser(User user) throws Exception;
+	//회원조회
+	public User getUser(String userId) throws Exception;
+	//회원목록 조회
+	public Map<String , Object> getUserList(Search search) throws Exception;
+	//회원목록 조회 (관리자)
+	public Map<String , Object> getUserListbyadmin(Search search) throws Exception; 
+	//회원 수정
+	public void updateUser(User user) throws Exception;
+	//아이디 유효성 체크
+	public boolean checkDuplication(String userId) throws Exception;
+	//포인트 조회
+	public User getpoint(int point) throws Exception;
+	//포인트 수정
+	public void updatepoint(User user)throws Exception;
+	//블랙리스트 추가
+	public void addBlacklist(User userId)throws Exception;
+	//블랙리스트 조회
+	public User getBlacklist(User userId)throws Exception;
+	//판매자 추가
+	public void addSeller(User user)throws Exception;
+	//판매자 수정
+	public void updateSeller(User user)throws Exception;
+	//판매자 평가 조회
+	public User getSellerEval(User user)throws Exception;
+	//판매자 등급 조회
+	public User getSalesGrade(String sellerCode)throws Exception;
+	
+	
+	
+	
+	
 }
