f39acc7039f434a04f286e014ffaef53ebf434d0
user <user@DESKTOP-74IMAPF>
2019-07-10 20:31:18 +0900

    userJY: CommunityMapper insert, select(get) 완료 update 수정 필요

--- Tget/src/main/java/com/tget/service/community/CommunityService.java
+++ Tget/src/main/java/com/tget/service/community/CommunityService.java
@@ -9,32 +9,45 @@ import com.tget.service.community.domain.Report;
 
 public interface CommunityService {
 	
-		// 게시글 등록
+		// 게시글 등록 ok
 		public void addContent(Content content) throws Exception;
-		// 신고 등록
+		
+		// 신고 등록 ok
 		public void addReport(Report report) throws Exception;
+		
+		// 댓글 등록 ok
+		public void addReply(Reply reply) throws Exception;
+		
 		// 게시글 조회
 		public Content getContent(int contentNo) throws Exception;
-		// 댓글 등록 
-		public void addReply(Reply reply) throws Exception;
+		
 		// 게시글 리스트 조회
 		public Map<String,Object> getContentList(Search search) throws Exception;
+		
 		// 댓글 리스트 조회
 		public Map<String,Object> getReplyList(Search search) throws Exception;
+		
 		// 신고 리스트 조회
 		public Map<String,Object> getReportList(Search search) throws Exception; //getTotalCount, getReportList 
+		
 		// 게시글 수정
 		public void updateContent(Content content) throws Exception;
+		
 		// 댓글 수정
 		public void updateReply(Reply reply) throws Exception;
+		
 		// 공감수
 		public void updateGoodCount(Map<String,Object> goodCount)throws Exception;
+		
 		// 비 공감수
 		public void updateBadCount(Map<String,Object> badCount)throws Exception;
+		
 		// 환불 게시판 환불 처리
 		public void updateRefund(Content content) throws Exception;
+		
 		// 길 찾기
 		public Map<String,Object> getSearchLoad(String load) throws Exception;
+		
 		// 날씨
 		public Map<String,Object> getSearchWeather(String weather) throws Exception;
 		
