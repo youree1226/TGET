e090efc6fb35b15aef302e30d9e7e0994cf4f3e7
user <user@DESKTOP-74IMAPF>
2019-07-10 13:54:50 +0900

    sw : commit 0710 13:54

--- Tget/src/main/java/com/tget/service/transaction/impl/TranDaoImpl.java
+++ Tget/src/main/java/com/tget/service/transaction/impl/TranDaoImpl.java
@@ -2,56 +2,70 @@ package com.tget.service.transaction.impl;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
 import com.tget.common.domain.Search;
 import com.tget.service.transaction.TranDao;
 import com.tget.service.transaction.domain.Transaction;
 
+@Repository("tranDaoImpl")
 public class TranDaoImpl implements TranDao{
-
+	
+	@Autowired
+	@Qualifier("sqlSessionTemplate")
+	private SqlSession sqlSession;
+	public void setSqlSession(SqlSession sqlSession) {
+		this.sqlSession = sqlSession;
+	}
+	
 	public TranDaoImpl() {
 		// TODO Auto-generated constructor stub
+		System.out.println(this.getClass());
 	}
 
 	@Override
 	public void insertTran(Transaction transaction) throws Exception {
 		// TODO Auto-generated method stub
-		
+		sqlSession.insert("TranMapper.insertTran", transaction);
 	}
 
 	@Override
 	public void updateDeliveryInfo(Transaction transaction) throws Exception {
 		// TODO Auto-generated method stub
-		
+		sqlSession.update("TranMapper.updateDeliveryInfo", transaction);
 	}
 
 	@Override
 	public void updateDeliveryAddr(Transaction transaction) {
 		// TODO Auto-generated method stub
-		
+		sqlSession.update("TranMapper.updateDeliveryAddr", transaction);
 	}
 
 	@Override
 	public void updateTranCode(Transaction transaction) throws Exception {
 		// TODO Auto-generated method stub
-		
+		sqlSession.update("TranMapper.updateTranCode", transaction);
 	}
 
 	@Override
 	public Transaction selectTran(int tranNo) throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		return sqlSession.selectOne("TranMapper.selectTran", tranNo);
 	}
 
 	@Override
 	public List<Transaction> selectListTran(Search search) throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		return sqlSession.selectList("TranMapper.selectListTran", search);
 	}
 
 	@Override
 	public int selectTotalCount(Search search) throws Exception {
 		// TODO Auto-generated method stub
-		return 0;
+		return sqlSession.selectOne("TranMapper.selectTotalCount", search);
 	}
 
 }
